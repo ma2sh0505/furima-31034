4b4a0062a9040518d6ae15d658406e6860ab5475
ma2sh0505 <ma284sh05@gmail.com>
2020-11-03 19:03:31 +0900

    テストコード修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -97,5 +97,10 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("First name kana is invalid")
     end
+    it "passwordは半角数字のみでは登録できない" do
+      @user.password = "1234567"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
   end
 end

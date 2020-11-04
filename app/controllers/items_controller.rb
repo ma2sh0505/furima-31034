54a2733008c4f240b8c80c162bed4cdc892d0b48
ma2sh0505 <ma284sh05@gmail.com>
2020-11-04 18:55:17 +0900

    DBに保存機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:introduction,:category_id,:condition_id,:price,:delivery_area_id,:delivery_burden_id,:delivery_day_id,:image).merge(user_id: current_user.id)
+  end
 end

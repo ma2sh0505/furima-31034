1f41a492136f1cd2f5e3bdfca829775120a4de67
ma2sh0505 <ma284sh05@gmail.com>
2020-11-04 15:53:51 +0900

    ルーティング記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.all
+  end
+
+  def create
+    @item = Item.new
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,22 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  has_one_attached :image
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_area
   belongs_to :delivery_burden
   belongs_to :delivery_day
-  
+
+   with_options presence: true do
+     validates :name
+     validates :introduction
+     validates :price
+     validates :image
+     validates :category_id, numericality: { other_than: 1}
+     validates :condition_id, numericality: { other_than: 1}
+     validates :delivery_area_id, numericality: { other_than: 1}
+     validates :delivery_burden_id, numericality: { other_than: 1}
+     validates :delivery_day_id, numericality: { other_than: 1}
+   end
 end

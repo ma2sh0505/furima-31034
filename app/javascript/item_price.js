dbc5f728a40dfb8061131325b0ed9ce5b96b9b5f
ma2sh0505 <ma284sh05@gmail.com>
2020-11-06 08:19:53 +0900

    JavaScript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
+  priceInput.addEventListener("click", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = priceInput.value /10
   })
-  priceInput.addEventListener("input", () => {
+  priceInput.addEventListener("click", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("profit");
     addTaxDom.innerHTML = priceInput.value * 0.9

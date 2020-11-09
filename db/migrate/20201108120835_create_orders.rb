6935429a7b59b1625ffd17113c36553b211601f3
ma2sh0505 <ma284sh05@gmail.com>
2020-11-09 11:29:47 +0900

    マイグレーションにカラム名の記述

--- db/migrate/20201108120835_create_orders.rb
+++ db/migrate/20201108120835_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.references :user,    foreigin_key: true
+      t.references :item,    foreigin_key: true
       t.timestamps
     end
   end
--- db/migrate/20201108120917_create_addresses.rb
+++ db/migrate/20201108120917_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string  :post_cord,          null: false
+      t.integer :prefecture_id,      null: false
+      t.string  :city,               null: false
+      t.string  :house_number,       null: false
+      t.string  :building_number
+      t.string  :phon_number,        null: false
+      t.references :order,    foreigin_key: true
       t.timestamps
     end
   end

13c06821b2e18315fe6b923eb956e47f8df0efba
ma2sh0505 <ma284sh05@gmail.com>
2020-11-10 18:05:46 +0900

    JavaScriptの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,6 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    
       if @order_address.valid?
         @order_address.save
         redirect_to root_path
